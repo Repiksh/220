f6c3ad931b4b963c8f5792e9732a81172735c1b7
Repiksh <repikscott@gmail.com>
2022-01-21 23:24:33 -0500

    Completed hw1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,51 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Scott Repik
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: There are a number of conversion problems, such as converting kilometers to miles, that need to have
+user input in order for the function to execute arithmetic and produce the correct result, depending on what is
+inputted
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length:"))
+    width = eval(input("Enter the width:"))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    height = eval(input("Enter the height:"))
+    width = eval(input("Enter the width:"))
+    length = eval(input("Enter the length:"))
+    volume = length*width*height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total = eval(input("Enter the total shots:"))
+    made = eval(input("Enter how many shots were made:"))
+    percent = made/total * 100
+    print(percent, "%")
 
 
 def coffee():
-    pass
+    pound = 10.5
+    shipping = .86
+    amount = eval(input("How many pounds of coffee would you like?"))
+    price = (amount*pound)+1.5+(amount*shipping)
+    print("Your total is:", price)
 
 
 def kilometers_to_miles():
-    pass
+    kilometer = .62
+    distance = eval(input("How many kilometers did you travel?"))
+    miles = round(distance*kilometer, 2)
+    print("That's", miles, "miles!")
 
 
 if __name__ == '__main__':
