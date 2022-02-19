be60ab2c168f26dbf3181e7bc055c1439bbf66d1
Repiksh <repikscott@gmail.com>
2022-02-18 23:47:47 -0500

    hw5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,72 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Scott Repik -Repiksh
+hw5.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
+    name = input("Enter a name: (First Last)")
+    name.split(" ")
+    first, last = name.split(" ")
+    print(last, ",", first)
 
 
 def company_name():
-    pass
+    name = input("Enter a three part domain:")
+    out_name = name.split(".")[1]
+    print(out_name)
 
 
 def initials():
-    pass
+    n = eval(input("How many students?"))
+    for i in range(n):
+        name = input("What is the name of the student")
+        name.split(" ")
+        first, last = name.split(" ")
+        print(first[0] + last[0])
 
 
 def names():
-    pass
+    name = input("List the names:")
+    name_split = name.split(", ")
+    name_list = name_split.split(" ")
+
+
+    start = ""
+
+    for name in name_list:
+
+        start = start + name[0].upper() + " "
+    print(start)
 
 
 def thirds():
-    pass
+    n = eval(input("Enter number of sentences:"))
+    sentence_list = []
+    for i in range(n):
+        sentence = input("Enter the sentence:")
+        sentence_list.append(list(sentence))
+    print(sentence_list[0::3])
 
 
 def word_average():
-    pass
+    sentence = input("Enter a sentence:")
+    words = sentence.split()
+    average = sum(len(word) for word in words) / len(words)
+    print(average)
 
 
 def pig_latin():
-    pass
+    sentence = input("Enter a sentence:").lower()
+    words = sentence.split()
+    for word in words:
+        print(word[1:] + word[0] + "ay", end=" ")
+    print()
 
 
 if __name__ == '__main__':
