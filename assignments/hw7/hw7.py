1ade18567c2539075eb5163b7e95ecdad710c511
Repiksh <repikscott@gmail.com>
2022-03-04 23:07:13 -0500

    hw7 wip

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,13 +1,13 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name:Scott Repik -Repiksh
+hw7.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
-<include one of the following>
+
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
@@ -16,7 +16,26 @@ def number_words(in_file_name, out_file_name):
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    wages = open(in_file_name, "r")
+    out = open(out_file_name, "w")
+    wages_list = wages.readlines()
+    for i in wages_list:
+        wage_split = i.split(" ")
+        first = wage_split[0].split()
+        last = wage_split[1].split()
+        payment = wage_split[2].split()
+        payment_list = [float(val) for val in payment]
+        print(payment_list)
+        hours = wage_split[3].split()
+        hours_list = [float(val) for val in hours]
+        bonus = [1.65]
+        int_wage = [a * b for a, b in zip(hours_list, payment_list)]
+        bonus_wage = [c * d for c, d in zip(bonus, hours_list)]
+        total = bonus_wage[0] + int_wage[0]
+        out.write(repr(first) + repr(last) + "total payment is " + repr(total) + "\n")
+    wages.close()
+    out.close()
+
 
 
 def calc_check_sum(isbn):
