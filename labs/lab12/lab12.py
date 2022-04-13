4c6b35d0e7586bf42e88ad19384d648ebe306539
Repiksh <repikscott@gmail.com>
2022-04-13 16:20:21 -0400

    lab 12 done

--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -8,41 +8,13 @@ lab12.py
 from random import *
 
 
-def find_and_remove(values, search_val):
+def find_and_remove_first(values, search_val):
     i = 0
     while i < len(values):
         if values[i] == search_val:
             values.pop(i)
             values.insert(i, "Scott")
         i += 1
-
-
-def read_data(file_name):
-    text = open(file_name, "r")
-    list_file = []
-    lines = text.readline()
-
-    while lines:
-        i = 0
-        no_new = lines.strip("\n")
-        lines_strip = no_new.split(" ")
-        while i < len(lines_strip):
-            is_int = int(lines_strip[i])
-            i += 1
-            list_file.append(is_int)
-        lines = text.readline()
-    text.close()
-
-    return list_file
-
-
-def is_in_linear(search_val, values):
-    i = 0
-    while i < len(values):
-        if values[i] == search_val:
-            return True
-        i += 1
-    return False
 
 
 def good_input():
@@ -84,8 +56,10 @@ def hi_lo_game():
     print("The number was: {}".format(secret))
 
 
-
-
 def main():
-    pass
+    hi_lo_game()
+    num_digits()
+    good_input()
+
+
 
